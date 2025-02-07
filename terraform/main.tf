9d321e3c28a486e9152402d8411d8dc51263011b
sushma-sm <gurajasushmamounika@gmail.com>
2025-02-07 05:34:10 +0000

    added terrfaoem files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
 provider "google" {
-  credentials = file("/home/gurajasushmamounika/DEMO/terraform/devops-practice-sm-42887ecbb300.json")
   project     = "devops-practice-sm"
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = "my-terraform-bucket-sp"
+  name     = "my-terraform-bucket-14"
   location = "US"
 }
