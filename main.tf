d9736d9a482231eed46b2775dc9c8c38a5911b86
sushma-sm <gurajasushmamounika@gmail.com>
2025-02-12 18:50:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 provider "google" {
-  project     = "devops-practice-sm"
-  region      = "us-central1"
+  project = "devops-practice-sm"
+  region  = "us-central1"
+}
+
+# Generate a random ID to append to the bucket name
+resource "random_id" "bucket_suffix" {
+  byte_length = 6
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = "my-terraform-bucket-abc4"
+  name     = "my-terraform-bucket-${random_id.bucket_suffix.hex}"
   location = "US"
 }
