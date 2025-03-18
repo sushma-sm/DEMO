2ddd5de8d62a7f187ca1b085305cd42d6af74af3
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-18 16:41:32 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,21 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootApplication
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class AppTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void helloEndpointTest() {
+        String response = this.restTemplate.getForObject("/", String.class);
+        assertThat(response).contains("Hi Sushma!");
     }
 }
-
-@RestController
-class HelloWorldController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hi Sushma! Your Dockerized container is up and running.\nThis project follows an end-to-end pipeline with Git, Docker, Ansible, and Terraform.";
-    }
-}
-
